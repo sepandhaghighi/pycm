79be414bbc8e50ed9f35484f6a7504cbc454e57f
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-01-27 17:09:48 +0330

    fix : testcases updated

--- pycm/test.py
+++ pycm/test.py
@@ -19,10 +19,18 @@ Actual
 <BLANKLINE>
 <BLANKLINE>
 <BLANKLINE>
+Overall Statistics :
+<BLANKLINE>
+Overall_ACC                                                      0.58333
+Overall_Kappa                                                    0.35484
+Strength_Of_Agreement                                            Fair
+<BLANKLINE>
+Class Statistics :
+<BLANKLINE>
 Classes                                                          0                       1                       2
 ACC(accuracy)                                                    0.83333                 0.75                    0.58333
 BM(Informedness or Bookmaker Informedness)                       0.77778                 0.22222                 0.16667
-DOR(Diagnostic odds ratio)                                       None                    4.0                     2.00003
+DOR(Diagnostic odds ratio)                                       None                    4.0                     2.0
 F1(F1 Score - harmonic mean of precision and sensitivity)        0.75                    0.4                     0.54545
 FDR(false discovery rate)                                        0.4                     0.5                     0.4
 FN(false negative/miss/Type II error)                            0                       2                       3
@@ -31,7 +39,8 @@ FOR(false omission rate)                                         0.0            
 FP(false positive/Type I error/false alarm)                      2                       1                       2
 FPR(fall-out or false positive rate)                             0.22222                 0.11111                 0.33333
 G(G-measure geometric mean of precision and sensitivity)         0.7746                  0.40825                 0.54772
-LR+(Positive likelihood ratio)                                   4.50005                 3.0                     1.50002
+K(Kappa)                                                         0.81395                 0.73913                 0.47368
+LR+(Positive likelihood ratio)                                   4.5                     3.0                     1.5
 LR-(Negative likelihood ratio)                                   0.0                     0.75                    0.75
 MCC(Matthews correlation coefficient)                            0.68313                 0.2582                  0.16903
 MK(Markedness)                                                   0.6                     0.3                     0.17143
@@ -41,6 +50,8 @@ P(Condition positive)                                            3              
 POP(Population)                                                  12                      12                      12
 PPV(precision or positive predictive value)                      0.6                     0.5                     0.6
 PRE(Prevalence)                                                  0.25                    0.25                    0.5
+RACC(Random Accuracy)                                            0.10417                 0.04167                 0.20833
+SOA(Strength of Agreement)                                       Almost Perfect          Substantial             Moderate
 TN(true negative/correct rejection)                              7                       8                       4
 TNR(specificity or true negative rate)                           0.77778                 0.88889                 0.66667
 TON(Test outcome negative)                                       7                       10                      7
@@ -62,11 +73,19 @@ Actual
 1                0.0            0.33333        0.66667
 2                0.33333        0.16667        0.5
 <BLANKLINE>
->>> cm.params()
+>>> cm.stat()
+Overall Statistics :
+<BLANKLINE>
+Overall_ACC                                                      0.58333
+Overall_Kappa                                                    0.35484
+Strength_Of_Agreement                                            Fair
+<BLANKLINE>
+Class Statistics :
+<BLANKLINE>
 Classes                                                          0                       1                       2
 ACC(accuracy)                                                    0.83333                 0.75                    0.58333
 BM(Informedness or Bookmaker Informedness)                       0.77778                 0.22222                 0.16667
-DOR(Diagnostic odds ratio)                                       None                    4.0                     2.00003
+DOR(Diagnostic odds ratio)                                       None                    4.0                     2.0
 F1(F1 Score - harmonic mean of precision and sensitivity)        0.75                    0.4                     0.54545
 FDR(false discovery rate)                                        0.4                     0.5                     0.4
 FN(false negative/miss/Type II error)                            0                       2                       3
@@ -75,7 +94,8 @@ FOR(false omission rate)                                         0.0            
 FP(false positive/Type I error/false alarm)                      2                       1                       2
 FPR(fall-out or false positive rate)                             0.22222                 0.11111                 0.33333
 G(G-measure geometric mean of precision and sensitivity)         0.7746                  0.40825                 0.54772
-LR+(Positive likelihood ratio)                                   4.50005                 3.0                     1.50002
+K(Kappa)                                                         0.81395                 0.73913                 0.47368
+LR+(Positive likelihood ratio)                                   4.5                     3.0                     1.5
 LR-(Negative likelihood ratio)                                   0.0                     0.75                    0.75
 MCC(Matthews correlation coefficient)                            0.68313                 0.2582                  0.16903
 MK(Markedness)                                                   0.6                     0.3                     0.17143
@@ -85,6 +105,8 @@ P(Condition positive)                                            3              
 POP(Population)                                                  12                      12                      12
 PPV(precision or positive predictive value)                      0.6                     0.5                     0.6
 PRE(Prevalence)                                                  0.25                    0.25                    0.5
+RACC(Random Accuracy)                                            0.10417                 0.04167                 0.20833
+SOA(Strength of Agreement)                                       Almost Perfect          Substantial             Moderate
 TN(true negative/correct rejection)                              7                       8                       4
 TNR(specificity or true negative rate)                           0.77778                 0.88889                 0.66667
 TON(Test outcome negative)                                       7                       10                      7
@@ -108,11 +130,11 @@ pycm.pycm.pycmError: Input Vectors Must Be The Same Length
 | .__/  \__, | \___||_| |_| |_|
 |_|     |___/
 <BLANKLINE>
-__     __     ___      ____
-\ \   / / _  / _ \    |___ \
- \ \ / / (_)| | | |     __) |
-  \ V /   _ | |_| | _  / __/
-   \_/   (_) \___/ (_)|_____|
+__     __     ___      _____
+\ \   / / _  / _ \    |___ /
+ \ \ / / (_)| | | |     |_ \
+  \ V /   _ | |_| | _  ___) |
+   \_/   (_) \___/ (_)|____/
 <BLANKLINE>
 <BLANKLINE>
 Repo : https://github.com/sepandhaghighi/pycm

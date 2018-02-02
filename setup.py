8b22b8d7b3bfa11d603021676c2c78c5af3d2772
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-02-02 22:19:41 +0330

    fix : setup file version modified

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pycm',
   packages = ['pycm'],
-  version = '0.3',
+  version = '0.4',
   description = 'Confusion Matrix In Python',
   long_description='Confusion Matrix In Python',
   author = 'Sepand Haghighi',
   author_email = 'sepand@qpage.ir',
   url = 'https://github.com/sepandhaghighi/pycm',
-  download_url = 'https://github.com/sepandhaghighi/pycm/tarball/v0.3',
+  download_url = 'https://github.com/sepandhaghighi/pycm/tarball/v0.4',
   keywords = ['confusion-matrix', 'python3','python','machine learning','ML'],
   install_requires=[
 	  'codecov',

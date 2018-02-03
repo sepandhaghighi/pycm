b407cba14b133c8e949bc03abb96a8304cf325c2
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-02-03 17:59:40 +0330

    fix : testcases modified

--- pycm/test.py
+++ pycm/test.py
@@ -173,6 +173,65 @@ True
 'None'
 >>> cm.F_beta(4)
 {0: 0.9622641509433962, 1: 0.34, 2: 0.504950495049505}
+>>> y_test = [5, 5, 5, 0, 0, 0]
+>>> y_pred = [5, 0, 0, 0, 0, 0]
+>>> cm=ConfusionMatrix(y_test, y_pred)
+>>> print(cm)
+Predict          0        5
+Actual
+0                3        0
+5                2        1
+<BLANKLINE>
+<BLANKLINE>
+<BLANKLINE>
+<BLANKLINE>
+Overall Statistics :
+<BLANKLINE>
+Overall_ACC                                                      0.66667
+Overall_Kappa                                                    0.33333
+Strength_Of_Agreement(Altman)                                    Fair
+Strength_Of_Agreement(Fleiss)                                    Poor
+Strength_Of_Agreement(Landis and Koch)                           Fair
+<BLANKLINE>
+Class Statistics :
+<BLANKLINE>
+Classes                                                          0                       5
+ACC(accuracy)                                                    0.66667                 0.66667
+BM(Informedness or Bookmaker Informedness)                       0.33333                 0.33333
+DOR(Diagnostic odds ratio)                                       None                    None
+ERR(Error Rate)                                                  0.33333                 0.33333
+F0.5(F0.5 Score)                                                 0.65217                 0.71429
+F1(F1 Score - harmonic mean of precision and sensitivity)        0.75                    0.5
+F2(F2 Score)                                                     0.88235                 0.38462
+FDR(false discovery rate)                                        0.4                     0.0
+FN(false negative/miss/Type II error)                            0                       2
+FNR(miss rate or false negative rate)                            0.0                     0.66667
+FOR(false omission rate)                                         0.0                     0.4
+FP(false positive/Type I error/false alarm)                      2                       0
+FPR(fall-out or false positive rate)                             0.66667                 0.0
+G(G-measure geometric mean of precision and sensitivity)         0.7746                  0.57735
+K(Kappa)                                                         0.42857                 0.63636
+LR+(Positive likelihood ratio)                                   1.5                     None
+LR-(Negative likelihood ratio)                                   0.0                     0.66667
+MCC(Matthews correlation coefficient)                            0.44721                 0.44721
+MK(Markedness)                                                   0.6                     0.6
+N(Condition negative)                                            3                       3
+NPV(negative predictive value)                                   1.0                     0.6
+P(Condition positive)                                            3                       3
+POP(Population)                                                  6                       6
+PPV(precision or positive predictive value)                      0.6                     1.0
+PRE(Prevalence)                                                  0.5                     0.5
+RACC(Random Accuracy)                                            0.41667                 0.08333
+SOA1(Strength of Agreement,Landis and Koch)                      Moderate                Substantial
+SOA2(Strength of Agreement,Fleiss)                               Intermediate to Good    Intermediate to Good
+SOA3(Strength of Agreement,Altman)                               Moderate                Good
+TN(true negative/correct rejection)                              1                       3
+TNR(specificity or true negative rate)                           0.33333                 1.0
+TON(Test outcome negative)                                       1                       5
+TOP(Test outcome positive)                                       5                       1
+TP(true positive/hit)                                            3                       1
+TPR(sensitivity, recall, hit rate, or true positive rate)        1.0                     0.33333
+<BLANKLINE>
 >>> cov.stop()
 >>> cov.save()
 

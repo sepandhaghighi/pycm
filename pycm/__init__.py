ad26c90c67c9361d49134bf5c0e50cd67cad377f
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-02-11 00:06:24 +0330

    fix : moves params for pycm_params file

--- pycm/__init__.py
+++ pycm/__init__.py
@@ -2,3 +2,4 @@
 from .pycm_func import *
 from .pycm_output import *
 from .pycm_obj import *
+from .pycm_param import *
---
+++ pycm/pycm_param.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+VERSION="0.5"
+
+PARAMS_DESCRIPTION={"TPR":"sensitivity, recall, hit rate, or true positive rate","TNR":"specificity or true negative rate",
+                   "PPV":"precision or positive predictive value","NPV":"negative predictive value",
+                   "FNR":"miss rate or false negative rate","FPR":"fall-out or false positive rate",
+                   "FDR":"false discovery rate","FOR":"false omission rate","ACC":"accuracy",
+                   "F1":"F1 Score - harmonic mean of precision and sensitivity","MCC":"Matthews correlation coefficient",
+                   "BM":"Informedness or Bookmaker Informedness","MK":"Markedness","LR+":"Positive likelihood ratio",
+                   "LR-":"Negative likelihood ratio","DOR":"Diagnostic odds ratio","TP":"true positive/hit",
+                    "TN":"true negative/correct rejection","FP":"false positive/Type I error/false alarm",
+                    "FN":"false negative/miss/Type II error","P":"Condition positive","N":"Condition negative",
+                    "TOP":"Test outcome positive","TON":"Test outcome negative","POP":"Population","PRE":"Prevalence",
+                    "G":"G-measure geometric mean of precision and sensitivity","RACC":"Random Accuracy",
+                    "F0.5":"F0.5 Score","F2":"F2 Score","ERR":"Error Rate"}

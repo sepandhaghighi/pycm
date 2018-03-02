6b013d9978775245372e0ac554f74e52e080c5ec
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-03-03 02:33:41 +0330

    fix : setup file modified

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup
 setup(
   name = 'pycm',
   packages = ['pycm'],
   version = '0.7',
   description = 'Confusion Matrix In Python',
-  long_description='Confusion Matrix In Python',
+  long_description='''In the field of machine learning and specifically the problem of statistical classification,
+   a confusion matrix, also known as an error matrix, is a specific table layout that allows visualization of
+    the performance of an algorithm, typically a supervised learning one (in unsupervised learning it is usually
+     called a matching matrix). Each row of the matrix represents the instances in a predicted class while each column
+      represents the instances in an actual class (or vice versa) pycm(python confusion matrix) is a multi class
+       confusion matrix library in python.''',
   author = 'Sepand Haghighi',
   author_email = 'sepand@qpage.ir',
   url = 'https://github.com/sepandhaghighi/pycm',
   download_url = 'https://github.com/sepandhaghighi/pycm/tarball/v0.7',
-  keywords = ['confusion-matrix', 'python3','python','machine learning','ML'],
+  keywords = "confusion-matrix python3 python machine_learning ML",
+  project_urls={
+    'Webpage': 'http://pycm.shaghighi.ir',
+    'Source': 'https://github.com/sepandhaghighi/pycm',
+     },
   install_requires=[
 	  'codecov',
       'art',
       ],
+  python_requires='>=2.7',
   classifiers=[
       'Development Status :: 4 - Beta',
       'Intended Audience :: Developers',
       'Natural Language :: English',
       'License :: OSI Approved :: MIT License',
       'Operating System :: OS Independent',
+      'Programming Language :: Python :: 3.3',
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
-      'Topic :: Text Processing :: Fonts',
+      'Topic :: Scientific/Engineering :: Information Analysis',
     ],
   license='MIT',
 )

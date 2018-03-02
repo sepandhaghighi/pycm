811421ce699588c30c8a3639aef09aa787b8a2be
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-03-02 15:29:28 +0330

    feat : jointEntropy added

--- pycm/pycm_func.py
+++ pycm/pycm_func.py
@@ -60,6 +60,20 @@ def cross_entropy_calc(TOP,P,POP):
         return -result
     except Exception :
         return "None"
+
+def joint_entropy_calc(classes,table,POP):
+    try:
+        result=0
+        classes.sort()
+        for i in classes:
+            for index,j in enumerate(classes):
+                p_prime=table[i][index]/POP[i]
+                if p_prime!=0:
+                    result+=p_prime*math.log(p_prime,2)
+        return -result
+    except Exception as e:
+        print(str(e))
+        return "None"
 def chi_square_calc(classes,table,TOP,P,POP):
     '''
     This function calculate chi-squared
@@ -558,6 +572,7 @@ def overall_statistics(RACC,TPR,PPV,TP,FN,FP,POP,P,TOP,classes,table):
     response_entropy=entropy_calc(TOP,POP)
     reference_entropy=entropy_calc(P,POP)
     cross_entropy = cross_entropy_calc(TOP,P,POP)
+    join_entropy = joint_entropy_calc(classes,table,POP)
     DF=DF_calc(classes)
     return {"Overall_ACC":overall_accuracy,"Kappa":overall_kappa,"Overall_RACC":overall_random_accuracy,
             "Strength_Of_Agreement(Landis and Koch)":kappa_analysis_koch(overall_kappa),
@@ -569,7 +584,7 @@ def overall_statistics(RACC,TPR,PPV,TP,FN,FP,POP,P,TOP,classes,table):
             "Scott_PI":PI,"Gwet_AC1":AC1,"Bennett_S":S,"Kappa Standard Error":kappa_SE,"Kappa 95% CI":kappa_CI,
             "Chi-Squared":chi_squared,"Phi-Squared":phi_squared,"Cramer_V":cramer_V,"Chi-Squared DF":DF,
             "95% CI":overall_accuracy_CI,"Standard Error":overall_accuracy_se,"Response Entropy":response_entropy,
-            "Reference Entropy":reference_entropy,"Cross Entropy":cross_entropy}
+            "Reference Entropy":reference_entropy,"Cross Entropy":cross_entropy,"Joint Entropy":join_entropy}
 def class_statistics(TP,TN,FP,FN):
     '''
     This function return all class statistics

5d4bdfd6dbfd7b51c0577f75186f89f8add36cb0
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-05-30 14:55:15 +0430

    fix : minor edit in testcases

--- pycm/test.py
+++ pycm/test.py
@@ -159,12 +159,6 @@ Webpage : http://pycm.shaghighi.ir
 'None'
 >>> reliability_calc(2,0.3)
 1.7
->>> kappa_analysis_koch("None")
-'None'
->>> kappa_analysis_fleiss("None")
-'None'
->>> kappa_analysis_altman("None")
-'None'
 >>> micro_calc({1:2,2:3},{1:1,2:4})
 0.5
 >>> micro_calc({1:2,2:3},None)

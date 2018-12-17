b310be209454cf0af4f828836b22c348cbea72cf
sepandhaghighi <sepand.haghighi@yahoo.com>
2018-12-17 17:02:21 +0330

    fix : pycm_profile moved to folder

--- pycm_profile.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from pycm import *
-import numpy as np
-
-x1 = np.random.randint(low=0,high=100,size=2000000)
-x2 = np.random.randint(low=0,high=100,size=2000000)
-
-cm = ConfusionMatrix(x1,x2)

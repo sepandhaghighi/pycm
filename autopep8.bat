f7e5c1a78cfd702fb9695c356a578d01f813411e
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-01-05 02:53:01 +0330

    fix : autopep8 fix

--- autopep8.bat
+++ autopep8.bat
@@ -1,1 +1,2 @@
 python -m autopep8 pycm --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose

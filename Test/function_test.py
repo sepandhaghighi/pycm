1aa124b50312aae6eec3be7db5ec81a7bc6a46b6
alirezazolanvari <alirezazolanvari93@gmail.com>
2019-02-21 17:04:18 +0330

    tests for recommend list added #112

--- Test/function_test.py
+++ Test/function_test.py
@@ -433,6 +433,8 @@ Example : online_help("J") or online_help(2)
 True
 >>> cm.imbalance
 True
+>>> set(cm.recommended_list) == set(IMBALANCED_RECOMMEND)
+True
 >>> cm = ConfusionMatrix(matrix={1:{1:60,2:9,3:1,4:0,5:0,6:0},2:{1:23,2:48,3:0,4:2,5:2,6:1},3:{1:11,2:5,3:1,4:0,5:0,6:0},4:{1:0,2:2,3:0,4:7,5:1,6:3},5:{1:2,2:1,3:0,4:0,5:4,6:2},6:{1:1,2:2,3:0,4:2,5:1,6:23}}) # Verified Case
 >>> cm.AM[1]
 27
@@ -440,7 +442,14 @@ True
 0.0630841121495327
 >>> cm.binary
 False
->>> cm = ConfusionMatrix(matrix={1:{1:295593,2:30516},2:{1:5108,2:295593}},transpose=True) 
+>>> set(cm.recommended_list) == set(IMBALANCED_RECOMMEND)
+True
+>>> cm = ConfusionMatrix(matrix={1:{1:295593,2:30516},2:{1:5108,2:295593}},transpose=True)
 >>> cm.imbalance
 False
+>>> set(cm.recommended_list) == set(BINARY_RECOMMEND)
+True
+>>> cm = ConfusionMatrix(matrix={1:{1:60,2:9,3:1,4:0,5:0,6:0},2:{1:23,2:48,3:0,4:2,5:2,6:1},3:{1:11,2:5,3:60,4:0,5:0,6:0},4:{1:0,2:2,3:0,4:60,5:1,6:3},5:{1:2,2:1,3:0,4:0,5:60,6:2},6:{1:1,2:2,3:0,4:2,5:1,6:60}})
+>>> set(cm.recommended_list) == set(MULTICLASS_RECOMMEND)
+True
 '''

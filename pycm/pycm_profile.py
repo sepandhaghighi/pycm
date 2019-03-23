6f3404bd931464c1e8bf286ea0f1a1de2e52e19a
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-03-23 19:26:19 +0430

    fix : pycm_profile updated #111

--- pycm/pycm_profile.py
+++ pycm/pycm_profile.py
@@ -6,3 +6,4 @@ x1 = np.random.randint(low=0, high=400, size=2000000)
 x2 = np.random.randint(low=0, high=400, size=2000000)
 
 cm = ConfusionMatrix(x1, x2)
+cp = Compare({"cm"+str(i):cm for i in range(100)})

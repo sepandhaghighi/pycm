c8689cf211a0da811d620315a4e835531687946d
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-03-28 19:13:58 +0430

    fix : static test in travis sets only for version 3.6

--- test.sh
+++ test.sh
@@ -2,7 +2,10 @@
   set -x
   python -m pytest Test --cov=pycm --cov-report=term
   python version_check.py
-  python -m vulture --min-confidence 80 --exclude=pycm,build,.eggs --sort-by-size .
-  python -m bandit -r pycm -s B311
-  python -m pydocstyle --match-dir=pycm
+  if [$TRAVIS_PYTHON_VERSION == '3.6']
+  then
+      python -m vulture --min-confidence 80 --exclude=pycm,build,.eggs --sort-by-size .
+	  python -m bandit -r pycm -s B311
+      python -m pydocstyle --match-dir=pycm
+  fi
   python -m cProfile -s cumtime pycm/pycm_profile.py

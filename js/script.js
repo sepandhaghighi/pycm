e52c6ab5926883a86c44b05b97f8283f3317546b
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-05-06 21:08:00 +0430

    paypal added to donate page

--- js/script.js
+++ js/script.js
@@ -2,19 +2,16 @@
 function redirect(flag){
     switch(flag){
         case 1:
-            window.open("https://saythanks.io/to/opem");
+            window.open("https://www.paypal.me/masijsm");
             break;
         case 2:
             swal("Bitocin Wallet","1XGr9qbZjBpUQJJSB6WtgBQbDTgrhPLPA");
-            break;
-        case 3:
-            window.open("https://beerpay.io/sepandhaghighi/lightsout");
             break;
         case 4:
             window.open("https://www.payping.ir/sepandhaghighi");
             break;
         default:
-            window.open("https://saythanks.io/to/opem");
+            window.open("https://www.paypal.me/masijsm");
             
     }
     

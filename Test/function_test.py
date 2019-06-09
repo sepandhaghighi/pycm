fe6d09088f31779e72bdd52ac3206fc95296c1b6
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-06-10 02:28:09 +0430

    fix : minor edit in testcases #209

--- Test/function_test.py
+++ Test/function_test.py
@@ -590,7 +590,6 @@ Example : online_help("J") or online_help(2)
 >>> cm.F1_Micro
 0.4444444444444444
 >>> cm = ConfusionMatrix(matrix = {"True":{"True":5,"False":1},"False":{"False":6,"True":2}}) # Verified Case
->>> cm.AGF
 >>> cm.AGF["True"]
 0.8197822947299411
 >>> cm.F2["True"]

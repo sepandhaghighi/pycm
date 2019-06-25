b001f4a7cbd3479756a8af74f8bb00028376b324
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-06-25 20:33:49 +0430

    fix : BENCHMARK_COLOR updated #221

--- pycm/pycm_param.py
+++ pycm/pycm_param.py
@@ -364,14 +364,14 @@ BENCHMARK_COLOR = {
     "PLRI" : {"Negligible": "Red","Poor": "Orange","Fair": "Yellow","Good": "Green","None": "White"},
     "NLRI" : {"Negligible": "Red","Poor": "Orange","Fair":  "Yellow","Good": "Green","None": "White"},
     "DPI" : {"Poor": "Red","Limited": "Orange","Fair": "Yellow","Good": "Green","None": "White"},
-    "AUCI" : {"Poor": "Red","Fair": "Orange","Good": "Yellow Green","Very Good": "Lawn Green","Excellent": "Green","None": "White"},
-    "MCCI" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Strong": "Lawn Green","Very Strong": "Green","None": "White"},
-    "SOA1(Landis & Koch)" : {"Poor": "Red","Slight": "Orange Red","Fair": "Orange","Moderate": "Yellow","Substantial": "Lawn Green","Almost Perfect": "Green","None": "White"},
-    "SOA2(Fleiss)" : {"Poor": "Red","Intermediate to Good": "Lawn Green","Excellent": "Green","None": "White"},
-    "SOA3(Altman)" : {"Poor": "Red","Fair": "Orange","Moderate": "Yellow","Good": "Lawn Green","Very Good": "Green","None": "White"},
-    "SOA4(Cicchetti)" : {"Poor": "Red","Fair": "Orange","Good": "Lawn Green","Excellent": "Green","None": "White"},
-    "SOA5(Cramer)" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Relatively Strong": "Yellow Green","Strong": "Lawn Green","Very Strong": "Green","None": "White"},
-    "SOA6(Matthews)" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Strong": "Lawn Green","Very Strong": "Green","None": "White"}
+    "AUCI" : {"Poor": "Red","Fair": "Orange","Good": "YellowGreen","Very Good": "LawnGreen","Excellent": "Green","None": "White"},
+    "MCCI" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Strong": "LawnGreen","Very Strong": "Green","None": "White"},
+    "SOA1(Landis & Koch)" : {"Poor": "Red","Slight": "OrangeRed","Fair": "Orange","Moderate": "Yellow","Substantial": "LawnGreen","Almost Perfect": "Green","None": "White"},
+    "SOA2(Fleiss)" : {"Poor": "Red","Intermediate to Good": "LawnGreen","Excellent": "Green","None": "White"},
+    "SOA3(Altman)" : {"Poor": "Red","Fair": "Orange","Moderate": "Yellow","Good": "LawnGreen","Very Good": "Green","None": "White"},
+    "SOA4(Cicchetti)" : {"Poor": "Red","Fair": "Orange","Good": "LawnGreen","Excellent": "Green","None": "White"},
+    "SOA5(Cramer)" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Relatively Strong": "YellowGreen","Strong": "LawnGreen","Very Strong": "Green","None": "White"},
+    "SOA6(Matthews)" : {"Negligible": "Red","Weak": "Orange","Moderate": "Yellow","Strong": "LawnGreen","Very Strong": "Green","None": "White"}
                 }
 
 TABLE_COLOR = {

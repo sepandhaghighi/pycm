a512905de4a9d60b52f419125aaac15027ceb5c5
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-06-26 16:20:11 +0430

    fix : testcases updated

--- Test/overall_test.py
+++ Test/overall_test.py
@@ -1037,6 +1037,9 @@ Y(Youden index)                                                  0.73333        
 dInd(Distance index)                                             0.26667                 0.67586                 0.54716
 sInd(Similarity index)                                           0.81144                 0.52209                 0.6131
 <BLANKLINE>
+>>> cm2 = ConfusionMatrix(y_actu, y_pred, sample_weight=np.array([2, 2, 2, 2, 3, 1, 1, 2, 2, 1, 1, 2]))
+>>> cm2 == cm
+True
 >>> cm = ConfusionMatrix([1,2,3,4],[1,2,3,"4"])
 >>> cm
 pycm.ConfusionMatrix(classes: ['1', '2', '3', '4'])

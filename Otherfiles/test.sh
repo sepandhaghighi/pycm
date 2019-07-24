59cd6a85918465b019766d6d82d8dcc6063bea44
sadrasabouri <sabouri.sadra@gmail.com>
2019-07-24 23:54:30 +0430

    Minimum confidence set to 65%

--- Otherfiles/test.sh
+++ Otherfiles/test.sh
@@ -4,7 +4,7 @@
   python Otherfiles/version_check.py
   if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
   then
-      python -m vulture --min-confidence 80 --exclude=pycm,build,.eggs --sort-by-size
+      python -m vulture --min-confidence 65 --exclude=pycm,build,.eggs --sort-by-size
       python -m bandit -r pycm -s B311
       python -m pydocstyle --match-dir=pycm
   fi

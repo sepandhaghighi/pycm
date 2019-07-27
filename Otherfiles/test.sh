3a1700667f4ef2552f10a29f0dc1be99db7ca976
sadrasabouri <sabouri.sadra@gmail.com>
2019-07-27 13:55:56 +0430

    add : TRAVIS check added.

--- Otherfiles/test.sh
+++ Otherfiles/test.sh
@@ -2,7 +2,15 @@
   set -x
   python -m pytest Test --cov=pycm --cov-report=term
   python Otherfiles/version_check.py
-  if [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]
+
+  #Check if we are in TRAVIS
+  isInTRAVIS=false
+  if [ "$CI" = 'true' ] && [ "$TRAVIS" = 'true' ]
+  then
+      $isInTRAVIS=true
+  fi
+
+  if [ "$isInTRAVIS" = 'false' ] || { [ "$isInTRAVIS" = 'true' ] && [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]; }
   then
       python -m vulture pycm/ Otherfiles/ setup.py --min-confidence 65 --exclude=build,.eggs --sort-by-size
       python -m bandit -r pycm -s B311

733505500fcf9543f46be4e17066dd8febb4f7bd
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-07-29 18:09:55 +0430

    fix : isINTRAVIS renamed to IS_IN_TRAVIS

--- Otherfiles/test.sh
+++ Otherfiles/test.sh
@@ -5,13 +5,13 @@
   python Otherfiles/version_check.py
 
   #Check if we are in TRAVIS
-  isInTRAVIS=false
+  IS_IN_TRAVIS=false
   if [ "$CI" = 'true' ] && [ "$TRAVIS" = 'true' ]
   then
-      isInTRAVIS=true
+      IS_IN_TRAVIS=true
   fi
 
-  if [ "$isInTRAVIS" = 'false' ] || { [ "$isInTRAVIS" = 'true' ] && [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]; }
+  if [ "$IS_IN_TRAVIS" = 'false' ] || { [ "$IS_IN_TRAVIS" = 'true' ] && [ "$TRAVIS_PYTHON_VERSION" = '3.6' ]; }
   then
       python -m vulture pycm/ Otherfiles/ setup.py --min-confidence 65 --exclude=build,.eggs --sort-by-size
       python -m bandit -r pycm -s B311

f67a626f6eff640f940eb0412c82f3910775359a
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-09-10 16:32:28 +0430

    fix : ICSI added to object #245

--- pycm/pycm_obj.py
+++ pycm/pycm_obj.py
@@ -629,6 +629,7 @@ def __class_stat_init__(cm):
     cm.OC = cm.class_stat["OC"]
     cm.OOC = cm.class_stat["OOC"]
     cm.AUPR = cm.class_stat["AUPR"]
+    cm.ICSI = cm.class_stat["ICSI"]
 
 
 def __overall_stat_init__(cm):

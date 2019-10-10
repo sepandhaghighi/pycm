d315398c52a667e18a9440ba3ca4a7b73ab99b5b
sadrasabouri <sabouri.sadra@gmail.com>
2019-10-10 04:01:58 +0330

    add : New tests added

--- Test/output_test.py
+++ Test/output_test.py
@@ -273,6 +273,8 @@ Actual
 >>> y_actu = [2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2, 0, 1, 0, 2, 1, 0, 0, 0, 1, 2, 4, 5]
 >>> y_pred = [2, 0, 2, 2, 0, 2, 2, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0, 0, 0, 0, 2, 5, 3]
 >>> cm = ConfusionMatrix(actual_vector=y_actu, predict_vector=y_pred)
+>>> cm.sparse_matrix
+>>> cm.sparse_normalized_matrix
 >>> cm.print_matrix()
 Predict 0       1       2       3       4       5
 Actual
@@ -333,6 +335,10 @@ Actual
 5         0.0       0.0       1.0       0.0
 <BLANKLINE>
 <BLANKLINE>
+>>> cm.sparse_normalized_matrix
+[{0: {0: 1.0, 2: 0.0, 3: 0.0, 5: 0.0}, 1: {0: 0.5, 2: 0.5, 3: 0.0, 5: 0.0}, 2: {0: 0.0, 2: 1.0, 3: 0.0, 5: 0.0}, 4: {0: 0.0, 2: 0.0, 3: 0.0, 5: 1.0}, 5: {0: 0.0, 2: 0.0, 3: 1.0, 5: 0.0}}, [0, 1, 2, 4, 5], [0, 2, 3, 5]]
+>>> cm.sparse_matrix
+[{0: {0: 8, 2: 0, 3: 0, 5: 0}, 1: {0: 3, 2: 3, 3: 0, 5: 0}, 2: {0: 0, 2: 8, 3: 0, 5: 0}, 4: {0: 0, 2: 0, 3: 0, 5: 1}, 5: {0: 0, 2: 0, 3: 1, 5: 0}}, [0, 1, 2, 4, 5], [0, 2, 3, 5]]
 >>> save_stat_data = cm.save_stat("test", sparse = True)
 >>> save_stat_data['Status']
 True

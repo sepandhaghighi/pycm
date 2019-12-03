c402d9eeb44f1abe4fedf16dd2a1d37e4d9c69d4
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-12-03 18:36:36 +0330

    doc : verified tests reference added

--- Test/function_test.py
+++ Test/function_test.py
@@ -484,17 +484,17 @@ Example : online_help("J") or online_help(2)
 ...
 >>> online_help(4)
 ...
->>> NIR_calc({'Class2': 804, 'Class1': 196},1000) # Verified Case
+>>> NIR_calc({'Class2': 804, 'Class1': 196},1000) # Verified Case - (Caret package)
 0.804
->>> cm = ConfusionMatrix(matrix={0:{0:3,1:1},1:{0:4,1:2}})   # Verified Case
+>>> cm = ConfusionMatrix(matrix={0:{0:3,1:1},1:{0:4,1:2}})   # Verified Case - (https://bit.ly/2DHQvjn)
 >>> cm.LS[1]
 1.1111111111111112
 >>> cm.LS[0]
 1.0714285714285714
->>> cm = ConfusionMatrix(matrix={"Class1":{"Class1":183,"Class2":13},"Class2":{"Class1":141,"Class2":663}})  # Verified Case
+>>> cm = ConfusionMatrix(matrix={"Class1":{"Class1":183,"Class2":13},"Class2":{"Class1":141,"Class2":663}})  # Verified Case - (Caret package)
 >>> cm.PValue
 0.000342386296143693
->>> cm = ConfusionMatrix(matrix={"Class1":{"Class1":4,"Class2":2},"Class2":{"Class1":2,"Class2":4}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={"Class1":{"Class1":4,"Class2":2},"Class2":{"Class1":2,"Class2":4}}) # Verified Case - (Delgado, Nunez-Gonzalez, 2018)
 >>> cm.Overall_CEN
 0.861654166907052
 >>> cm.Overall_MCEN
@@ -503,23 +503,23 @@ Example : online_help("J") or online_help(2)
 0.4150374992788437
 >>> cm.IS["Class2"]
 0.4150374992788437
->>> cm = ConfusionMatrix(matrix={1:{1:5,2:0,3:0},2:{1:0,2:10,3:0},3:{1:0,2:300,3:0}})  # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:5,2:0,3:0},2:{1:0,2:10,3:0},3:{1:0,2:300,3:0}})  # Verified Case - (Delgado, Nunez-Gonzalez, 2018)
 >>> cm.Overall_CEN
 0.022168905807495587
 >>> cm.Overall_MCC
 0.3012440235352457
 >>> cm.CBA
 0.3440860215053763
->>> cm = ConfusionMatrix(matrix={1:{1:1,2:3,3:0,4:0},2:{1:9,2:1,3:0,4:0},3:{1:0,2:0,3:100,4:0},4:{1:0,2:0,3:0,4:200}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:1,2:3,3:0,4:0},2:{1:9,2:1,3:0,4:0},3:{1:0,2:0,3:100,4:0},4:{1:0,2:0,3:0,4:200}}) # Verified Case - (Branco et al., 2017)
 >>> cm.RCI
 0.9785616782831341
->>> cm = ConfusionMatrix(matrix={1:{1:1,2:0,3:3},2:{1:0,2:100,3:0},3:{1:0,2:0,3:200}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:1,2:0,3:3},2:{1:0,2:100,3:0},3:{1:0,2:0,3:200}}) # Verified Case - (Branco et al., 2017)
 >>> cm.RCI
 0.9264007150415143
->>> cm = ConfusionMatrix(matrix={1:{1:5,2:0,3:0},2:{1:0,2:10,3:0},3:{1:0,2:300,3:0}})
+>>> cm = ConfusionMatrix(matrix={1:{1:5,2:0,3:0},2:{1:0,2:10,3:0},3:{1:0,2:300,3:0}})  # Verified Case - (Branco et al., 2017)
 >>> cm.RCI
 0.3675708571923818
->>> cm = ConfusionMatrix(matrix={1:{1:12806,2:26332},2:{1:5484,2:299777}},transpose=True) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:12806,2:26332},2:{1:5484,2:299777}},transpose=True) # Verified Case - (Bekkar et al., 2013)
 >>> cm.AUC[1]
 0.8097090079101759
 >>> cm.GI[1]
@@ -532,7 +532,7 @@ Example : online_help("J") or online_help(2)
 0.6194180158203517
 >>> cm.BM[1]
 0.6194180158203517
->>> cm = ConfusionMatrix(matrix={1:{1:13182,2:30516},2:{1:5108,2:295593}},transpose=True) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:13182,2:30516},2:{1:5108,2:295593}},transpose=True) # Verified Case - (Bekkar et al., 2013)
 >>> cm.AUC[1]
 0.8135728157964055
 >>> cm.GI[1]
@@ -545,12 +545,12 @@ Example : online_help("J") or online_help(2)
 0.627145631592811
 >>> cm.BM[1]
 0.627145631592811
->>> cm = ConfusionMatrix(matrix={1:{1:60,2:9,3:1,4:0,5:0,6:0},2:{1:23,2:48,3:0,4:2,5:2,6:1},3:{1:11,2:5,3:1,4:0,5:0,6:0},4:{1:0,2:2,3:0,4:7,5:1,6:3},5:{1:2,2:1,3:0,4:0,5:4,6:2},6:{1:1,2:2,3:0,4:2,5:1,6:23}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:60,2:9,3:1,4:0,5:0,6:0},2:{1:23,2:48,3:0,4:2,5:2,6:1},3:{1:11,2:5,3:1,4:0,5:0,6:0},4:{1:0,2:2,3:0,4:7,5:1,6:3},5:{1:2,2:1,3:0,4:0,5:4,6:2},6:{1:1,2:2,3:0,4:2,5:1,6:23}}) # Verified Case - (https://bit.ly/2YdvM01)
 >>> cm.AM[1]
 27
 >>> cm.BCD[1]
 0.0630841121495327
->>> cm = ConfusionMatrix(matrix={1:{1:9,2:3,3:0},2:{1:3,2:5,3:1},3:{1:1,2:1,3:4}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:9,2:3,3:0},2:{1:3,2:5,3:1},3:{1:1,2:1,3:4}}) # Verified Case -- (https://bit.ly/2r80R9t)
 >>> cm.CI95
 (0.48885185570907297, 0.8444814776242603)
 >>> cm.SE
@@ -587,7 +587,7 @@ Example : online_help("J") or online_help(2)
 0.35714285714285715
 >>> cm.Overall_ACC
 0.6666666666666666
->>> cm = ConfusionMatrix(matrix={1:{1:495,0:405},0:{0:8645,1:455}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:495,0:405},0:{0:8645,1:455}}) # Verified Case - (Garcia et al., 2009)
 >>> cm.ACC[1]
 0.914
 >>> cm.TNR[1]
@@ -608,30 +608,30 @@ Example : online_help("J") or online_help(2)
 0.5016
 >>> cm.GM[1]
 0.722841614740048
->>> cm = ConfusionMatrix(matrix={1:{1:22,0:18},0:{1:2,0:14}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:22,0:18},0:{1:2,0:14}}) # Verified Case - (https://bit.ly/2LiCZXB)
 >>> cm.C
 0.36170212765957444
 >>> cm.Chi_Squared
 8.429166666666667
->>> cm = ConfusionMatrix(matrix={0:{0:42,1:7},1:{1:114,0:203}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={0:{0:42,1:7},1:{1:114,0:203}}) # Verified Case - (https://bit.ly/2LiCZXB)
 >>> cm.Q[0]
 0.5422773393461104
->>> cm = ConfusionMatrix(matrix={1:{1:828,0:72},0:{0:8918,1:182}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:828,0:72},0:{0:8918,1:182}}) # Verified Case - (Batuwita, Palade, 2009)
 >>> cm.AGM[1]
 0.9640451296531609
 >>> cm.GM[1]
 0.9495261976375375
->>> cm = ConfusionMatrix(matrix={1:{1:882,0:18},0:{0:8372,1:728}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:882,0:18},0:{0:8372,1:728}}) # Verified Case - (Batuwita, Palade, 2009)
 >>> cm.AGM[1]
 0.935458742218606
 >>> cm.GM[1]
 0.9495261976375375
->>> cm = ConfusionMatrix([1,2,3,2,3,3,1,2,2],[2,2,1,2,1,3,2,3,2]) # Verified Case
+>>> cm = ConfusionMatrix([1,2,3,2,3,3,1,2,2],[2,2,1,2,1,3,2,3,2])
 >>> cm.F1_Macro
 0.35555555555555557
 >>> cm.F1_Micro
 0.4444444444444444
->>> cm = ConfusionMatrix(matrix = {1:{1:5,0:1},0:{0:6,1:2}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix = {1:{1:5,0:1},0:{0:6,1:2}})
 >>> cm.AGF[1]
 0.8197822947299411
 >>> cm.F2[1]
@@ -643,7 +643,7 @@ Example : online_help("J") or online_help(2)
 0.9636363636363636
 >>> cm.OOC[1]
 0.9383838571303771
->>> cm = ConfusionMatrix(matrix={1:{1:63,0:1},0:{0:50,1:2}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:63,0:1},0:{0:50,1:2}})
 >>> cm.TI(alpha=1,beta=1)[1]
 0.9545454545454546
 >>> cm.TI(alpha=0.5,beta=0.5)[1] == cm.F1[1]
@@ -654,7 +654,7 @@ True
 0.7777777777777778
 >>> cm.TI(alpha=2,beta=8)[0]
 0.8064516129032258
->>> cm = ConfusionMatrix(matrix={1:{1:22,0:54},0:{1:1,0:57}},transpose=True) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:22,0:54},0:{1:1,0:57}},transpose=True) # Verified Case -- (https://bit.ly/34KcVfB)
 >>> cm.TPR[1]
 0.9565217391304348
 >>> cm.CI("TPR",0.05)[1][1][0]
@@ -753,14 +753,14 @@ True
 0.6692157009292735
 >>> cm.CI("Overall ACC",binom_method="wilson")[1][0]
 0.5048971938717156
->>> SE = AUC_SE_calc(0.88915,279,527) # Verified Case
+>>> SE = AUC_SE_calc(0.88915,279,527) # Verified Case -- (https://bit.ly/2qblMrE)
 >>> SE
 0.011116012490627622
 >>> CI_calc(0.88915,SE)[0]
 0.8673626155183699
 >>> CI_calc(0.88915,SE)[1]
 0.9109373844816301
->>> cm = ConfusionMatrix(matrix={1:{1:135,0:48},0:{0:2014,1:1067}}) # Verified Case
+>>> cm = ConfusionMatrix(matrix={1:{1:135,0:48},0:{0:2014,1:1067}}) # Verified Case -- (Steyerberg et al., 2011, p. 792)
 >>> cm.TP[1]
 135
 >>> cm.FP[1]

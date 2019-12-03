76cc8a4f844addcca1410acd0750b84f941b134e
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-12-04 02:01:28 +0330

    fix : namespace optimized

--- pycm/__init__.py
+++ pycm/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 """PyCM modules."""
-from .pycm_class_func import *
-from .pycm_overall_func import *
-from .pycm_output import *
-from .pycm_obj import *
-from .pycm_compare import *
-from .pycm_util import *
-from .pycm_interpret import *
-from .pycm_ci import *
+from .pycm_param import PYCM_VERSION
+from .pycm_error import *
+from .pycm_output import pycm_help, online_help
+from .pycm_obj import ConfusionMatrix
+from .pycm_compare import Compare
 __version__ = PYCM_VERSION

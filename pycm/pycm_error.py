a58f6696b51ca647339ec756a34e90e2c7260691
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-12-04 02:04:29 +0330

    fix : autopep8 fix

--- pycm/pycm_error.py
+++ pycm/pycm_error.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """ConfusionMatrix, Compare and CI errors."""
 
+
 class pycmVectorError(Exception):
     """Vector error class."""
 
     pass
+
 
 class pycmMatrixError(Exception):
     """Matrix error class."""
@@ -16,6 +18,7 @@ class pycmCIError(Exception):
     """CI error class."""
 
     pass
+
 
 class pycmCompareError(Exception):
     """Compare error class."""

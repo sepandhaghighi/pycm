9d5d4592d25d84ba24d0e783ffb35fa6f24638d7
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-03-17 18:01:24 +0330

    fix : Python 3.8 added to setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',

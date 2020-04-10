d735a41fabd3a6d0f7cd3c457eaf0f8b74c76444
geet <win.12894@gmail.com>
2020-04-10 09:37:57 +0200

    removed old lines from pycm_overall_func.py

--- pycm/pycm_handler.py
+++ pycm/pycm_handler.py
@@ -106,7 +106,6 @@ def __overall_stat_init__(cm):
     cm.PPV_Micro = cm.overall_stat["PPV Micro"]
     cm.F1_Macro = cm.overall_stat["F1 Macro"]
     cm.F1_Micro = cm.overall_stat["F1 Micro"]
-    cm.F1_Weighted = cm.overall_stat["F1 Weighted"]
     cm.Overall_RACC = cm.overall_stat["Overall RACC"]
     cm.Overall_RACCU = cm.overall_stat["Overall RACCU"]
     cm.PI = cm.overall_stat["Scott PI"]

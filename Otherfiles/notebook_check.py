647635123db32c31efebc1df0d349ac30182a84c
sadrasabouri <sabouri.sadra@gmail.com>
2020-09-09 16:07:20 +0430

    change : Notebooks -> Document.

--- Otherfiles/notebook_check.py
+++ Otherfiles/notebook_check.py
@@ -18,7 +18,7 @@ EXTENSION = ".ipynb"
 
 if __name__ == "__main__":
     tprint("PYCM","bulbhead")
-    tprint("NOTEBOOKS","bulbhead")
+    tprint("Document","bulbhead")
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
     print("Processing ...")
     for index, notebook in enumerate(NOTEBOOKS_LIST):

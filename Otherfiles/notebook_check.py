1fb41987e3a3333e0cdc289e5bdba99f7321535b
sadrasabouri <sabouri.sadra@gmail.com>
2020-09-09 16:03:51 +0430

    add : main part of notebook_check.py added..

--- Otherfiles/notebook_check.py
+++ Otherfiles/notebook_check.py
@@ -15,3 +15,17 @@ NOTEBOOKS_LIST = [
     "Example8"]
 
 EXTENSION = ".ipynb"
+
+if __name__ == "__main__":
+    tprint("PYCM","bulbhead")
+    tprint("NOTEBOOKS","bulbhead")
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    print("Processing ...")
+    for index, notebook in enumerate(NOTEBOOKS_LIST):
+        path = os.path.join("Document", notebook)
+        with open(path + EXTENSION) as f:
+            nb = nbformat.read(f, as_version=4)
+            ep.preprocess(nb, {'metadata': {'path': 'Document/'}})
+        with open(path + EXTENSION, 'w', encoding='utf-8') as f:
+            nbformat.write(nb, f)
+        print("{0}.{1} [OK]".format(str(index + 1), notebook))

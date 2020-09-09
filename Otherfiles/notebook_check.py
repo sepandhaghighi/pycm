f8776b8e4b762986a255b6bffca55c85f5f40e48
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-09-10 01:28:05 +0430

    fix : minor edit in notebook_check script encoding

--- Otherfiles/notebook_check.py
+++ Otherfiles/notebook_check.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     for index, notebook in enumerate(NOTEBOOKS_LIST):
         ep = ExecutePreprocessor(timeout=6000, kernel_name='python3')
         path = os.path.join("Document", notebook)
-        with open(path + EXTENSION,"r","utf-8") as f:
+        with open(path + EXTENSION,"r",encoding="utf-8") as f:
             nb = nbformat.read(f, as_version=4)
             ep.preprocess(nb, {'metadata': {'path': 'Document/'}})
         with open(path + EXTENSION, 'w', encoding='utf-8') as f:

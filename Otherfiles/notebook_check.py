1c7ddaf10c5083eaa348809726225b30a3dc2a65
sepandhaghighi <sepand.haghighi@yahoo.com>
2020-09-10 01:22:45 +0430

    fix : minor edit in notebook_check script

--- Otherfiles/notebook_check.py
+++ Otherfiles/notebook_check.py
@@ -23,10 +23,8 @@ if __name__ == "__main__":
     for index, notebook in enumerate(NOTEBOOKS_LIST):
         ep = ExecutePreprocessor(timeout=6000, kernel_name='python3')
         path = os.path.join("Document", notebook)
-        with open(path + EXTENSION,"rb") as f:
-            nb_bytes = f.read()
-            nb_text = nb_bytes.decode("utf-8")
-            nb = nbformat.read(nb_text, as_version=4)
+        with open(path + EXTENSION,"r","utf-8") as f:
+            nb = nbformat.read(f, as_version=4)
             ep.preprocess(nb, {'metadata': {'path': 'Document/'}})
         with open(path + EXTENSION, 'w', encoding='utf-8') as f:
             nbformat.write(nb, f)

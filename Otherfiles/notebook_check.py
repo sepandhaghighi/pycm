f035add9c98413b629578370eefc1c94f9ac7ed4
sadrasabouri <sabouri.sadra@gmail.com>
2020-09-09 16:01:31 +0430

    add : NOTEBOOKS_LIST and EXTENSION added..

--- Otherfiles/notebook_check.py
+++ Otherfiles/notebook_check.py
@@ -2,3 +2,16 @@ import os
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from art import tprint
+
+NOTEBOOKS_LIST = [
+    "Document",
+    "Example1",
+    "Example2",
+    "Example3"
+    "Example4",
+    "Example5"
+    "Example6",
+    "Example7",
+    "Example8"]
+
+EXTENSION = ".ipynb"

9f41e1aad9c042478da4a8797621567d6fbe2a06
sadrasabouri <sabouri.sadra@gmail.com>
2020-10-03 22:48:16 +0330

    fix : autopep8ed.

--- pycm/pycm_util.py
+++ pycm/pycm_util.py
@@ -457,7 +457,7 @@ def add_number_label(ax, classes, matrix, cmap, number_label_cmap):
     diff_matrix_max = float(diff_matrix.max())
     for i in range(len(classes)):
         for j in range(len(classes)):
-            color = cmap(round(diff_matrix[i][j]/diff_matrix_max))
+            color = cmap(round(diff_matrix[i][j] / diff_matrix_max))
             if number_label_cmap is not None:
                 color = number_label_cmap(
                     float(matrix[i][j]) / float(matrix.max()))
@@ -469,7 +469,14 @@ def add_number_label(ax, classes, matrix, cmap, number_label_cmap):
                     color=color)
 
 
-def axes_gen(ax, classes, matrix, title, cmap, number_label, number_label_cmap):
+def axes_gen(
+        ax,
+        classes,
+        matrix,
+        title,
+        cmap,
+        number_label,
+        number_label_cmap):
     """
     Add Number labels to Confusion matrix plot.
 

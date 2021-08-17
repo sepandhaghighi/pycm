1b5520368a8dfb7339cef3ec14a89ddd37f8ab31
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-08-17 19:09:32 +0430

    donate page updated

--- js/script.js
+++ js/script.js
@@ -5,10 +5,7 @@ function redirect(flag){
             window.open("https://www.paypal.me/masijsm");
             break;
         case 2:
-            swal("Bitocin Wallet","1XGr9qbZjBpUQJJSB6WtgBQbDTgrhPLPA");
-            break;
-        case 4:
-            window.open("https://www.payping.ir/sepandhaghighi");
+            window.open("https://www.pycm.ir/wallets.html");
             break;
         default:
             window.open("https://www.paypal.me/masijsm");

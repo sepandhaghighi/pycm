aa5403ec024d44b086d23258c2e3932f8ae06ee8
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-11-15 16:08:37 +0330

    fix : Python 3.10 added to setup.py #391

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',

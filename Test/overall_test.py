0937969c589fdc480377d0cb27c7aeae4785e27e
sadrasabouri <sabouri.sadra@gmail.com>
2022-06-20 23:48:07 +0430

    add : tests for numpy added.

--- Test/overall_test.py
+++ Test/overall_test.py
@@ -1526,4 +1526,16 @@ L3       4        6        1
 L2       1        2        3
 <BLANKLINE>
 <BLANKLINE>
+>>> matrix = np.array([[1,2,3],[4,6,1],[1,2,3]])
+>>> cm = ConfusionMatrix(matrix=matrix)
+>>> cm.print_matrix()
+Predict 0       1       2
+Actual
+0       1       2       3
+<BLANKLINE>
+1       4       6       1
+<BLANKLINE>
+2       1       2       3
+<BLANKLINE>
+<BLANKLINE>
 """

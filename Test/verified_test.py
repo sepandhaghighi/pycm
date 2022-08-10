2bd805e1719f8f67fbeddf7d5bf43db9dc43df6f
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-10 14:21:29 +0430

    fix : BB verified test added #349

--- Test/verified_test.py
+++ Test/verified_test.py
@@ -392,4 +392,9 @@ True
 5
 >>> cm2.HD[0]
 5
+>>> cm1 = ConfusionMatrix(matrix = {1:{1:2,0:2},0:{0:778,1:2}})  # Verified Case -- (https://bit.ly/3BVdNBp)
+>>> cm1.BB
+0.5
+>>> cm2 = ConfusionMatrix(matrix = {1:{1:2,0:3},0:{0:775,1:4}})  # Verified Case -- (https://bit.ly/3BVdNBp)
+0.3333333333333333
 """

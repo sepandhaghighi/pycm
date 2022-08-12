37fe739eef578689d2d592c884ed0d169b1d192f
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-12 18:24:43 +0430

    fix : doc_run script updated

--- Otherfiles/doc_run.bat
+++ Otherfiles/doc_run.bat
@@ -3,7 +3,7 @@ call python -m art text "Doc Run Script"
 for %%f in (Document\*.ipynb) do (
 echo %%f is running!
 call python -m nbconvert %%f --execute --clear-output --log-level=ERROR
-if ERRORLEVEL 1 (echo %%f Failed! & exit /b 0) else (echo Done!)
+if ERRORLEVEL 1 (echo %%f Failed! & exit /b 0) else (echo %%f Done!)
 echo --------------------------
 )
 

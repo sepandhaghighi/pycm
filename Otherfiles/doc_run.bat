d60a46dc20e2f565927d377b9d7b7a0c859d5deb
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-12 17:58:45 +0430

    fix : doc_run script updated

--- Otherfiles/doc_run.bat
+++ Otherfiles/doc_run.bat
@@ -6,3 +6,9 @@ call python -m nbconvert %%f --execute --clear-output --log-level=ERROR
 if ERRORLEVEL 1 (echo %%f Failed! & exit /b 0) else (echo Done!)
 echo --------------------------
 )
+
+copy Document\Document_Files\cm1.csv Otherfiles\test.csv
+copy Document\Document_Files\cm1.html Otherfiles\test.html
+copy Document\Document_Files\cm1.obj Otherfiles\test.obj
+copy Document\Document_Files\cm1.pycm Otherfiles\test.pycm
+copy Document\Document_Files\cp.comp Otherfiles\test.comp

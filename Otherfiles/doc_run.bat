bd5d73100309e71abcf53932a4aca199a93bbe83
Sepand Haghighi <sepand.haghighi@yahoo.com>
2022-08-13 01:01:53 +0430

    Doc HTML (#446)
    * doc : toc tags added to Document
    
    * doc : toc meta added to Document
    
    * doc : doc_to_html script added
    
    * doc : minor edit in toc skip tag
    
    * doc : doc_run script added
    
    * fix : minor edit in doc_to_html script
    
    * fix : doc_to_html script updated
    
    * fix : doc_run script moved to Otherfiles folder
    
    * fix : doc_run script updated
    
    * fix : doc_to_html script updated
    
    * fix : doc_run script updated
    
    * fix : doc_run script updated

---
+++ Otherfiles/doc_run.bat
@@ -0,0 +1,14 @@
+@echo off
+call python -m art text "Doc Run Script"
+for %%f in (Document\*.ipynb) do (
+echo %%f is running!
+call python -m nbconvert %%f --execute --clear-output --log-level=ERROR
+if ERRORLEVEL 1 (echo %%f Failed! & exit /b 0) else (echo %%f Done!)
+echo --------------------------
+)
+
+copy Document\Document_Files\cm1.csv Otherfiles\test.csv
+copy Document\Document_Files\cm1.html Otherfiles\test.html
+copy Document\Document_Files\cm1.obj Otherfiles\test.obj
+copy Document\Document_Files\cm1.pycm Otherfiles\test.pycm
+copy Document\Document_Files\cp.comp Otherfiles\test.comp

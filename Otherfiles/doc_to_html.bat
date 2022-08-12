a70f8449dfc2b7d3e914fed5183ce33b87855643
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-12 15:21:02 +0430

    doc : doc_to_html script added

---
+++ Otherfiles/doc_to_html.bat
@@ -0,0 +1,10 @@
+@echo off
+
+if not exist "doc_html" mkdir doc_html
+copy ..\Document\*.ipynb doc_html
+cd doc_html
+python -m nbconvert --to html_toc --execute Document.ipynb --no-prompt --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags={\"html_hide\"}
+move Document.html index.html
+del Document.ipynb
+for %%f in (*.ipynb) do (python -m nbconvert --to html --execute %%f --no-prompt & del %%f)
+cd ..

7f9391f84755651f3a0c66900358a93fc8386643
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-12 17:42:55 +0430

    fix : doc_to_html script updated

--- Otherfiles/doc_to_html.bat
+++ Otherfiles/doc_to_html.bat
@@ -1,11 +1,22 @@
 @echo off
 
+call python -m art text "Doc 2 HTML"
 if not exist "doc_html" mkdir doc_html
 for %%f in (doc_html\*) do (del %%f)
 copy ..\Document\*.ipynb doc_html
 cd doc_html
-python -m nbconvert --to html_toc --execute Document.ipynb --no-prompt --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags={\"html_hide\"}
+echo --------------------------
+echo Document.ipynb is running!
+call python -m nbconvert --to html_toc --execute Document.ipynb --no-prompt --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags={\"html_hide\"} --log-level=ERROR
 move Document.html index.html
+if ERRORLEVEL 1 (echo Document.ipynb Failed! & cd .. & exit /b 0) else (echo Document.ipynb Done!)
+echo --------------------------
 del Document.ipynb
-for %%f in (*.ipynb) do (python -m nbconvert --to html --execute %%f --no-prompt & del %%f)
+for %%f in (*.ipynb) do (
+echo %%f is running!
+call python -m nbconvert --to html --execute %%f --no-prompt --log-level=ERROR
+if ERRORLEVEL 1 (echo %%f Failed! & cd .. & exit /b 0) else (echo %%f Done!)
+echo --------------------------
+del %%f
+)
 cd ..

e6fe4db3d7d9f07d23f7a5cd611f7b41afc759fc
alirezazolanvari <alireza.zolanvari93@gmail.com>
2022-11-16 15:43:33 +0100

    add area method error

--- pycm/pycm_param.py
+++ pycm/pycm_param.py
@@ -69,6 +69,7 @@ CLASS_NUMBER_ERROR = "Number of the classes is lower than 2"
 CLASSES_ERROR = "Used classes is not a subset of matrix's classes."
 COMPARE_FORMAT_ERROR = "The input type is supposed to be dictionary but it's not!"
 CLASSES_LENGHT_ERROR = "Classes length is not equal to the array length."
+AREA_METHOD_ERROR = "The numeric integral method can only be selected between 'trapezoidal' and 'midpoint'!"
 
 COMPARE_TYPE_ERROR = "The input is supposed to consist of pycm.ConfusionMatrix object but it's not!"
 COMPARE_DOMAIN_ERROR = "The domain of all ConfusionMatrix objects must be same! The sample size or the number " \

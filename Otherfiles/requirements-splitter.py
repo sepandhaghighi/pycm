0aaf9ce629289e1d1f1ca61f86de5f1515934c60
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-01-01 00:34:27 +0330

    fix : requirements-splitter.py script updated

--- Otherfiles/requirements-splitter.py
+++ Otherfiles/requirements-splitter.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 """Requirements splitter."""
 
+PLOT_LIB_LIST = ["matplotlib", "seaborn"]
 test_req = ""
+plot_req = ""
 
 with open('dev-requirements.txt', 'r') as f:
     for line in f:
+        for lib in PLOT_LIB_LIST:
+            if line.find(lib) != -1:
+                plot_req += line
+                break
         if '==' not in line:
             test_req += line
 
 with open('test-requirements.txt', 'w') as f:
     f.write(test_req)
+
+with open('plot-requirements.txt', 'w') as f:
+    f.write(plot_req)

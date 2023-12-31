72866b11a55dd79febff2b5c1fda00ba4bba7711
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-01-01 00:44:36 +0330

    fix : plot_error_test.py test file added

---
+++ Test/plot_error_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+>>> from pycm import *
+>>> y_act = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+>>> y_pre = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 2, 0, 1, 2, 2, 2, 2]
+>>> cm = ConfusionMatrix(y_act, y_pre)
+>>> cm.print_matrix()
+Predict 0       1       2
+Actual
+0       9       3       0
+<BLANKLINE>
+1       3       5       1
+<BLANKLINE>
+2       1       1       4
+<BLANKLINE>
+<BLANKLINE>
+>>> ax = cm.plot()
+Traceback (most recent call last):
+    ...
+ImportError: No module named 'matplotlib'
+>>> ax = cm.plot(plot_lib='seaborn')
+Traceback (most recent call last):
+    ...
+ImportError: No module named 'matplotlib'
+"""

24b229e0db1de8d815b3646a133c1da415f17128
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-10-08 10:54:35 +0330

    fix : pycm_test --> basic_test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - `Python 3.12` added to `test.yml`
 - Warning and error messages updated
 - `pycm_util.py` renamed to `utils.py`
+- `pycm_test.py` renamed to `basic_test.py`
 ## [4.0] - 2023-06-07
 ### Added
 - `pycmMultiLabelError` class
--- pycm/__main__.py
+++ pycm/__main__.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     if len(args) > 1:
         if args[1].upper() == "TEST":
             error_flag = doctest.testfile(
-                "pycm_test.py",
+                "basic_test.py",
                 optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
                 | doctest.IGNORE_EXCEPTION_DETAIL,
                 verbose=False)[0]

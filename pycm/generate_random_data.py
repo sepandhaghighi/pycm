831ed6a9197395070c836f6e4274af93d54ac5ab
alirezazolanvari <alireza.zolanvari93@gmail.com>
2025-04-28 18:03:30 +0200

    add scenario

--- pycm/generate_random_data.py
+++ pycm/generate_random_data.py
@@ -3,6 +3,41 @@
 This file contains a function to generate a random confusion matrix based on given class percentages and total population.
 """
 import numpy as np
+from enum import Enum
+
+
+class Scenario(Enum):
+    UNIFORM = "uniform"
+    MAJORITY_CLASS = "majority_class"
+    MINORITY_CLASS = "minority_class"
+
+
+def _generate_class_percentages(num_classes: int, scenario: Scenario) -> list:
+    """
+    Generate class percentages based on the given scenario.
+
+    Args:
+        num_classes (int): Number of classes.
+        scenario (Scenario): The scenario to generate percentages for.
+
+    Returns:
+        list: List of percentages for each class.
+    """
+    if scenario == Scenario.UNIFORM:
+        # Equal percentage for all classes
+        return [100 / num_classes] * num_classes
+    elif scenario == Scenario.MAJORITY_CLASS:
+        # One class has a majority class, others share the rest equally
+        majority_percentage = (100 / num_classes) * 5
+        remaining_percentage = (100 - majority_percentage) / (num_classes - 1)
+        return [majority_percentage] + [remaining_percentage] * (num_classes - 1)
+    elif scenario == Scenario.MINORITY_CLASS:
+        # One class has a minority percentage, others share the rest equally
+        minority_percentage = (100 / num_classes) * 0.2
+        remaining_percentage = (100 - minority_percentage) / (num_classes - 1)
+        return [minority_percentage] + [remaining_percentage] * (num_classes - 1)
+    else:
+        raise ValueError("Invalid scenario")
 
 
 def _calculate_class_counts(
@@ -75,3 +110,21 @@ def generate_confusion_matrix(
         confusion_matrix[i, :] = counts
 
     return confusion_matrix
+
+
+def generate_confusion_matrix_with_scenario(
+    num_classes: int, total_population: int, scenario: Scenario = Scenario.UNIFORM
+) -> np.ndarray:
+    """
+    Generate a random confusion matrix based on the given scenario.
+
+    Args:
+        num_classes (int): Number of classes.
+        total_population (int): Total number of samples.
+        scenario (Scenario): The scenario to generate the confusion matrix for. Defaults to UNIFORM.
+
+    Returns:
+        numpy.ndarray: A confusion matrix.
+    """
+    class_percentages = _generate_class_percentages(num_classes, scenario)
+    return generate_confusion_matrix(class_percentages, total_population)

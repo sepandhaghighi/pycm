428a9ce3f240827f86b7b503c451b0e1801e6a47
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-18 11:44:53 +0330

    fix : Python 3.14 added to setup.py

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',

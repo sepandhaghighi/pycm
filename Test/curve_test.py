ab616bb73d5b2b3a37c82795f4a78c7856889e21
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-12 01:04:38 +0330

    fix : tests updated

--- Test/curve_test.py
+++ Test/curve_test.py
@@ -32,10 +32,35 @@ True
 [2, 1]
 >>> crv.thresholds
 [0.1, 0.2, 0.35, 0.4, 0.6, 0.65, 0.8, 0.9]
+>>> crv.plot_x_axis
+'thresholds'
+>>> crv.plot_y_axis
+'F1'
 >>> crv.data[2]["F1"]
 [0.6666666666666666, 0.8, 0.8, 0.5, 0.6666666666666666, 0.6666666666666666, 0.6666666666666666, 0.0]
 >>> crv.data[1]["F1"]
 [0.6666666666666666, 0.6666666666666666, 0.8, 0.8, 0.8, 0.5, 0.6666666666666666, 0.6666666666666666]
+>>> crv.data[2]["thresholds"] == crv.thresholds
+True
+>>> crv.data[1]["thresholds"] == crv.thresholds
+True
+>>> crv = Curve(actual_vector=np.array([1, 1, 2, 2]), probs=np.array([[0.1, 0.9], [0.4, 0.6], [0.35, 0.65], [0.8, 0.2]]), classes=[2, 1], y_axis="thresholds", x_axis="PPV")
+>>> crv
+pycm.Curve(classes: [2, 1])
+>>> crv.binary
+True
+>>> crv.classes
+[2, 1]
+>>> crv.thresholds
+[0.1, 0.2, 0.35, 0.4, 0.6, 0.65, 0.8, 0.9]
+>>> crv.plot_x_axis
+'PPV'
+>>> crv.plot_y_axis
+'thresholds'
+>>> crv.data[2]["PPV"]
+[0.5, 0.6666666666666666, 0.6666666666666666, 0.5, 1.0, 1.0, 1.0, 'None']
+>>> crv.data[1]["PPV"]
+[0.5, 0.5, 0.6666666666666666, 0.6666666666666666, 0.6666666666666666, 0.5, 1.0, 1.0]
 >>> crv.data[2]["thresholds"] == crv.thresholds
 True
 >>> crv.data[1]["thresholds"] == crv.thresholds

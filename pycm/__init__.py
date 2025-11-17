af41913d3e3aefcc2c31780b76fcebf743e0fb55
alirezazolanvari <alireza.zolanvari93@gmail.com>
2025-11-17 11:13:14 +0100

    add p-curve

--- pycm/__init__.py
+++ pycm/__init__.py
@@ -7,7 +7,7 @@ from .distance import DistanceType
 from .cm import ConfusionMatrix
 from .compare import Compare
 from .multilabel_cm import MultiLabelCM
-from .curve import Curve, ROCCurve, PRCurve
+from .curve import Curve, ROCCurve, PRCurve, PCurve
 from .generate_random_data import generate_confusion_matrix, generate_confusion_matrix_with_scenario
 from .generate_random_data import ClassDistributionScenario, run_report_benchmark
 __version__ = PYCM_VERSION

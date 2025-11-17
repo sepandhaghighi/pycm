4883c69c668501d1562f61a20806d25890fca6fd
alirezazolanvari <alireza.zolanvari93@gmail.com>
2025-11-17 11:13:31 +0100

    add p-curve tests

--- Test/curve_test.py
+++ Test/curve_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
->>> from pycm import Curve, ROCCurve, PRCurve
+>>> from pycm import Curve, ROCCurve, PRCurve, PCurve
 >>> import numpy as np
 >>> crv = Curve(actual_vector=np.array([1, 1, 2, 2]), probs=np.array([[0.1, 0.9], [0.4, 0.6], [0.35, 0.65], [0.8, 0.2]]), classes=[2, 1])
 >>> crv
@@ -157,4 +157,23 @@ True
 [1.0, 0.5, 1.0, 1.0, 0.5, 0.5, 0.5]
 >>> crv.data[2]["PPV"]
 [0.5, 0.5, 0.6666666666666666, 0.6666666666666666, 1.0, 1.0, 1.0]
+>>> crv = PCurve(actual_vector=np.array([0, 1, 1, 2, 2]), probs=np.array([[0.01, 0.09, 0.9], [0.01, 0.09, 0.9], [0.1, 0.3, 0.6], [0.2, 0.35, 0.45], [0.1, 0.7, 0.2]]), classes=[0, 2, 1])
+>>> crv
+pycm.PCurve(classes: [0, 2, 1])
+>>> crv.classes
+[0, 2, 1]
+>>> crv.binary
+False
+>>> crv.data[2]["thresholds"]
+[0, 0.01, 0.09, 0.1, 0.2, 0.3, 0.35, 0.45, 0.6, 0.7]
+>>> crv.data[1]["thresholds"]
+[0, 0.01, 0.09, 0.1, 0.2, 0.3, 0.35, 0.45, 0.6, 0.7, 0.9]
+>>> crv.data[0]["thresholds"]
+[0, 0.01, 0.09, 0.1, 0.2]
+>>> crv.title
+'P Curve'
+>>> crv.plot_x_axis
+'thresholds'
+>>> crv.plot_y_axis
+'PPV'
 """

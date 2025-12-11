610b7fbf7e0cfed059ccb4195a5d66d93afa3c3c
Fabian Hausmann <fabian.hausmann@cssb-hamburg.de>
2025-12-11 16:17:24 +0100

    Move tests to overall tests

--- Test/overall_test.py
+++ Test/overall_test.py
@@ -1750,4 +1750,24 @@ Zero       2          0
 >>> cm = ConfusionMatrix([0, 0, 1, 0], y_pred_act, threshold=activation, transpose=2, metrics_off=True)
 >>> assert isclose(cm.brier_score(pos_class=1), 0.224225, abs_tol=ABS_TOL, rel_tol=REL_TOL)
 >>> assert isclose(cm.brier_score(pos_class=0), 0.509225, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> import numpy as np
+>>> maxval = 10e17
+>>> matrix = np.array([[maxval,maxval],[maxval,2]]).astype(int)
+>>> cm = ConfusionMatrix(matrix=matrix)
+>>> cm.class_stat["MCC"]
+{0: -0.5, 1: -0.5}
+>>> cm.class_stat["RACC"]
+{0: 0.4444444444444445, 1: 0.11111111111111112}
+>>> cm.class_stat["Q"]
+{0: -1.0, 1: -1.0}
+>>> cm.class_stat["F1"]
+{0: 0.5, 1: 2e-18}
+>>> cm.class_stat["OOC"]
+{0: 0.5, 1: 2e-18}
+>>> cm.overall_stat["Chi-Squared"]
+np.float64(7.499999999999999e+17)
+>>> cm.overall_stat["Overall MCC"]
+np.float64(-0.5000000000000001)
+>>> cm.overall_stat["Bangdiwala B"]
+0.19999999999999998
 """

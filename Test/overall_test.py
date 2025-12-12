537583f78c56f09148b4098a928c3905e45f01cc
Fabian Hausmann <fabian.hausmann@cssb-hamburg.de>
2025-12-12 09:27:13 +0100

    Fix test on other platforms

--- Test/overall_test.py
+++ Test/overall_test.py
@@ -1754,20 +1754,17 @@ Zero       2          0
 >>> maxval = 10e17
 >>> matrix = np.array([[maxval,maxval],[maxval,2]]).astype(int)
 >>> cm = ConfusionMatrix(matrix=matrix)
->>> cm.class_stat["MCC"]
-{0: -0.5, 1: -0.5}
->>> cm.class_stat["RACC"]
-{0: 0.4444444444444445, 1: 0.11111111111111112}
->>> cm.class_stat["Q"]
-{0: -1.0, 1: -1.0}
->>> cm.class_stat["F1"]
-{0: 0.5, 1: 2e-18}
->>> cm.class_stat["OOC"]
-{0: 0.5, 1: 2e-18}
->>> cm.overall_stat["Chi-Squared"]
-np.float64(7.499999999999999e+17)
->>> cm.overall_stat["Overall MCC"]
-np.float64(-0.5000000000000001)
->>> cm.overall_stat["Bangdiwala B"]
-0.19999999999999998
+>>> assert isclose(cm.class_stat["MCC"][0], -0.5, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["MCC"][1], -0.5, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["RACC"][0], 0.4444444444444445, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["RACC"][1], 0.11111111111111112, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["Q"][0], -1.0, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["Q"][1], -1.0, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["F1"][0], 0.5, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["F1"][1], 2e-18, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["OOC"][0], 0.5, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.class_stat["OOC"][1], 2e-18, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.overall_stat["Chi-Squared"], 7.499999999999999e+17, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.overall_stat["Overall MCC"], -0.5, abs_tol=ABS_TOL, rel_tol=REL_TOL)
+>>> assert isclose(cm.overall_stat["Bangdiwala B"], 0.19999999999999998, abs_tol=ABS_TOL, rel_tol=REL_TOL)
 """
